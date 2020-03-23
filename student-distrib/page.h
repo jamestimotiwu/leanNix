516cd24c4699c7a5341db8b5fa79aea4b2a071b2
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-03-23 11:29:19 -0400

    Init paging

--- student-distrib/page.h
+++ student-distrib/page.h
@@ -1,6 +1,14 @@
+/* page.h - Defines for various page_tables, structure of page descriptor
+ * and selectors
+ * vim:ts=4 noexpandtab
+ */
+
 #include "types.h"
 
-typedef struct page
+#define NUM_PAGES   1024
+#define PAGE_SIZE   4096
+
+typedef struct __attribute__((packed)) page_entry
 {
     uint32_t present    : 1;
     uint32_t rw         : 1;
@@ -9,20 +17,10 @@ typedef struct page
     uint32_t dirty      : 1;  
     uint32_t unused     : 7; 
     uint32_t frame      : 20; 
-} page_t;
+} page_entry_t;
 
-typedef struct page_table
-{
-    page_t pages[1024];                 /* 1024 page table entries in a page table */
-} page_table_t;
+page_entry_t page_table[NUM_PAGES] __attribute__((aligned(PAGE_SIZE)));
 
-typedef struct page_directory
-{
-    page_table* tables[1024];           /* 1024 page directory entries in page directory */
-    uint32_t physical_mem_table[1024];
-    uint32_t physical_address;
-} page_directory_t;
+page_entry_t page_dir[NUM_PAGES] __attribute__((aligned(PAGE_SIZE)));
 
-void switch_page_dir(page_directory_t* page_dir);
-
-page_t* get_page(uint32_t address, int make, page_directory_t* dir);
+page_entry_t page_vmem[NUM_PAGES] __attribute__((aligned(PAGE_SIZE)));
