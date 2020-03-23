da52373c6ed22169f23801fbba3a2c8619f30074
Wang, Benjamin Fen <bfwang2@illinois.edu>
2020-03-23 16:35:09 -0500

    fixed rtc magic numbers

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -3,9 +3,9 @@
 void rtc_int(){
 
   cli();				/* Disable interrupts */
-	outb(0x8C, 0x70);	/* Select and read register C */
-	inb(0x71);			/* Reset contents in CMOS port */
-	send_eoi(8);		/* Send EOI for IRQ8 */
+	outb(REG_B, RTC_PORT);	/* Select and read register C */
+	inb(CMOS_PORT);			/* Reset contents in CMOS port */
+	send_eoi(RTC_IRQ);		/* Send EOI for IRQ8 */
   //test_interrupts();
   printf("testing if rtc works");
 	sti();
@@ -14,10 +14,10 @@ void rtc_init(){
 
   char prev;
   cli();						/* Disable interrupts */
-  outb(0x8B, 0x70);			/* Select register B, disable non-maskable interrupt(NMI) */
-  prev = inb(0x71);			/* Read value of register B */
-  outb(0x8B, 0x70);			/* Set index again because read resets index in register */
-  outb((prev | 0x40), 0x71);	/* Write previous value | 0x40 to turn bit 6 of register in enable ints*/
+  outb(REG_B, RTC_PORT);			/* Select register B, disable non-maskable interrupt(NMI) */
+  prev = inb(CMOS_PORT);			/* Read value of register B */
+  outb(REG_B, RTC_PORT);			/* Set index again because read resets index in register */
+  outb((prev | 0x40), CMOS_PORT);	/* Write previous value | 0x40 to turn bit 6 of register in enable ints*/
   sti();						/* Enable interrupts */
-  enable_irq(8);
+  enable_irq(RTC_IRQ);
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -6,6 +6,14 @@
 #include "i8259.h"
 #include "lib.h"
 
+#define RTC_IRQ   8
+#define RTC_PORT  0x70
+#define CMOS_PORT 0x71
+#define REG_A     0x8A
+#define REG_B     0x8B
+#define REG_C     0x8C
+
+
 void rtc_int();
 void rtc_init();
 
