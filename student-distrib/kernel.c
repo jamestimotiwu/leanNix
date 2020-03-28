e2686d7cb80f5e5ca7d178a78f2892ae3d6dcf21
Michael Jamrozy <mjamro3@illinois.edu>
2020-03-28 11:55:03 -0500

    started work on file system

--- student-distrib/kernel.c
+++ student-distrib/kernel.c
@@ -158,6 +158,7 @@ void entry(unsigned long magic, unsigned long addr) {
 
     /* Initialize devices, memory, filesystem, enable device interrupts on the
      * PIC, any other initialization stuff... */
+	fs_init();
 
     /* Enable interrupts */
     /* Do not enable the following until after you have set up your
