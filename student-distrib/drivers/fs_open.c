b78249d87e5912689b923b3f3ffbe43b23d890bf
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-03-28 12:10:50 -0400

    Implement get dir entry, file open and close

--- student-distrib/drivers/fs_open.c
+++ student-distrib/drivers/fs_open.c
@@ -4,6 +4,14 @@
 
 #include "fs.h"
 #include "../types.h"
+#include "../lib.h"
+
+boot_block_t *fs;
+
+int32_t init_fs(uint32_t boot_block_addr) {
+    /* Set file system metadata block to address of boot block structure */
+    fs = (boot_block_t *) boot_block_addr;
+}
 
  /* get_dir_entry
   *   DESCRIPTION: Reads directory entry given a file_name and dir_entry
@@ -13,10 +21,19 @@
   *   SIDE EFFECTS:
   */
 int32_t get_dir_entry(const uint8_t* file_name, dir_entry_t* dir_entry) {
+    dir_entry_t* dir_entries = fs->dir_entry;
+    int i;
+    
     /* Iterate over dir entries in boot block for matching file name */
-    /* If found populate dir_entry accordingly */
+    for (i = 0; i < fs->dentry_count; i++) {
+        /* If filename match, populate dir_entry accordingly */
+        if (strncmp(file_name, dir_entries[i].filename, FILENAME_CHAR_LIMIT) {
+            memcpy(dir_entry, &dir_entries[i], sizeof(dir_entry));
+            return 0;
+        }
+    }
     /* Return -1 if filename dne */
-    return 0;
+    return -1;
 }
 
 /* fs_open
@@ -26,8 +43,12 @@ int32_t get_dir_entry(const uint8_t* file_name, dir_entry_t* dir_entry) {
  *   SIDE EFFECTS:
  */
 int32_t fs_open(const uint8_t* file_name) {
-    /* Check if file exist calling get_dir_entry */
-    /* Convert file to inode */
+    dir_entry_t* dir;
+
+    /* Validate file exist calling get_dir_entry */
+    if (get_dir_entry(file_name, dir_entry) == -1)
+        return -1;
+
     return 0;
 }
 
