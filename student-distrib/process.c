fcbb1a74140bb02a6d7ebcb10582930ebea6d38a
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-04-09 14:57:40 -0400

    Parsing command

--- student-distrib/process.c
+++ student-distrib/process.c
@@ -5,9 +5,29 @@
 #include "types.h"
 #include "drivers/fs.h"
 #include "page.h"
+#include "lib.h"
 
 int32_t process_execute(const uint8_t* command) {
-    /* Deconstr command arg */
+    /* Get filename from command */
+    uint8_t* filename;
+    int i;
+    int command_len;
+
+    /* Check if command has a filename */
+    command_len = strlen(command);
+    if (command_len == 0) {
+        return -1;
+    }
+
+    /* Get filename from command */
+    i = 0;
+    while (command[i] != ' ' || i < command_len) {
+        filename[i] = command[i];
+        i++;
+    }
+
+    /* Check if file exists with fs_open */
+
     /* Check exec magic headers */
 
     uint32_t proc_num = 0;
@@ -20,3 +40,7 @@ int32_t process_execute(const uint8_t* command) {
 int32_t process_halt(uint8_t status) {
     return 0;
 }
+
+//int8_t command_read(const uint8_t* command) {
+//
+//}
