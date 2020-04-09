aaa11cfaefb2bf542250e100269a28404002a0bf
James Timotiwu <17231755+jamestimotiwu@users.noreply.github.com>
2020-04-09 15:19:00 -0400

    Dereferencing warning in parse

--- student-distrib/process.c
+++ student-distrib/process.c
@@ -7,21 +7,21 @@
 #include "page.h"
 #include "lib.h"
 
+static int32_t command_read(const uint8_t* command, const uint8_t* arg);
+
 int32_t process_execute(const uint8_t* command) {
     /* Get filename from command */
-    uint8_t* filename[32];
+    uint8_t filename[32];
+    uint8_t args[128];
 
     /* Get filename, check if there is a file in command argument */
     if (command_read(command, filename) == 0) {
         return -1;
     }
 
-    while (command_read(const uint8_t* command))
-
-    i = 0;
-    while (command[i] != ' ' || i < command_len) {
-        filename[i] = command[i];
-        i++;
+    /* Getargs for rest of command */
+    if (command_read(command, args) == 0) {
+        return -1;
     }
 
     /* Check if file exists with fs_open */
@@ -50,24 +50,26 @@ int32_t command_read(const uint8_t* command, const uint8_t* arg) {
     int i;
     
     /* Check if command has anything remaining or valid */
-    command_len = strlen(command);
+    command_len = strlen((const int8_t*) command);
     if (command_len == 0) {
         return 0;
     }
 
     /* Strip leading whitespace */
     i = 0;
-    while (command[i] == ' ') {
+    while (command[i] == ' ' || i < command_len) {
         i++;
     }
 
     /* Get command arg from command */
+    command = &command[i];
+    i = 0;
     while (command[i] != ' ' || i < command_len) {
-        arg[i] = arg[i]
+        arg[i] = command[i];
     }
 
     /* Set new starting address for rest of command */
-    command = command[i];
+    command = &command[i];
 
     /* return len of argument parsed*/
     return i;
